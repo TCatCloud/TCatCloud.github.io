6c8ea9e79680de73cec4243822ddb65c025399d5
Moexin <moexin@foxmail.com>
2020-01-22 20:43:20 +0800

    V1.0.3
    V1.0.3

---
+++ env.js
@@ -0,0 +1,10 @@
+window.v2board = {
+  // 站点标题
+  title: 'TCat云服务',
+  // API
+  host: 'https://api.tcat.li',
+  // 主题
+  theme: '1',
+  // 背景
+  background_url: 'https://cdn.jsdelivr.net/gh/MoexinCDN/Picture@master/CatBG1.jpg'
+}
